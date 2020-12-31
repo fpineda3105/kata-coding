a3a65d7bf4543c2ad05b8b870ee0eafdb61be366
Fernando Pineda <fernandopineda3105@gmail.com>
2020-12-31 07:32:59 -0500

    Refactored Javascript Kata Hamming numbers using Dynamic Programming

--- javascript/hammingNumbers.test.js
+++ javascript/hammingNumbers.test.js
@@ -1,21 +1,15 @@
-function hamming(n) {    
-    let length = 1
-    let candidate = 1;
-    while (length < n) {
-        candidate++;
-        if (isHammingNumber(candidate)) {                
-            length++;
-        }        
+function hamming (n) {
+    var seq = [1];
+    var i2 = 0, i3 = 0, i5 = 0;
+    for (var i = 1; i < n; i++) {
+      var x = Math.min(2 * seq[i2], 3 * seq[i3], 5 * seq[i5]);
+      seq.push(x);
+      if (2 * seq[i2] <= x) i2++;  
+      if (3 * seq[i3] <= x) i3++;
+      if (5 * seq[i5] <= x) i5++;
     }
-    return candidate;
-}
-function isHammingNumber(num) {
-    while (num % 5 === 0) num /= 5;
-    while (num % 3 === 0) num /= 3;
-    while (num % 2 === 0) num /= 2;
-
-    return num == 1;
-}
+    return seq[n-1];
+  }
 
 
 //----- Test Cases ---//
