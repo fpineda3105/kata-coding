ac7c17848778248d2776bfcdfc2b8ce840c71fba
Fernando Pineda <fernandopineda3105@gmail.com>
2021-11-14 20:56:26 -0500

    added new test case for testing CI elixir pipeline

--- elixir/test/basic_collections_test.exs
+++ elixir/test/basic_collections_test.exs
@@ -3,6 +3,7 @@ defmodule BasicCollectionsTest do
   import Collections
 
   test "should double an array" do
+    assert double_array([1, 2]) == [2, 4]
     assert double_array([1, 2, 3, 4, 5]) == [2, 4, 6, 8, 10]
     assert double_array([5, 8, 12, 19, 22]) == [10, 16, 24, 38, 44]
   end
